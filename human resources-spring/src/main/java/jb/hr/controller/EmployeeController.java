7236bca386e96d0c392bf2934c5c9294c547ab08
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2023-12-24 14:31:27 -0600

    list of employees with REST controller

--- human resources-spring/src/main/java/jb/hr/controller/EmployeeController.java
+++ human resources-spring/src/main/java/jb/hr/controller/EmployeeController.java
@@ -1,13 +1,17 @@
 package jb.hr.controller;
 
+import jb.hr.model.Employee;
 import jb.hr.service.EmployeeService;
 import jb.hr.service.IEmployeeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 //htpp://localhost:8080/hr-app/
@@ -19,4 +23,11 @@ public class EmployeeController {
 
     @Autowired
     private IEmployeeService employeeService;
+    // htpp://localhost:8080/hr-app/employees
+    @GetMapping("/employees")
+    public List<Employee> obtainEmployees(){
+        var employees = employeeService.listEmployees();
+        employees.forEach(employee -> logger.info(employee.toString()));
+        return employees;
+    }
 }
