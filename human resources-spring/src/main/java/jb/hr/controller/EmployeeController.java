466435a878076e6c8f40cdcca6871ebd6aa0878b
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2023-12-24 14:25:04 -0600

    employee controller

--- human resources-spring/src/main/java/jb/hr/controller/EmployeeController.java
+++ human resources-spring/src/main/java/jb/hr/controller/EmployeeController.java
@@ -1,2 +1,22 @@
-package jb.hr.controller;public class EmployeeController {
+package jb.hr.controller;
+
+import jb.hr.service.EmployeeService;
+import jb.hr.service.IEmployeeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//htpp://localhost:8080/hr-app/
+@RequestMapping("hr-app")
+@CrossOrigin(value = "http://localhost:3000")
+public class EmployeeController {
+    private static final Logger logger=
+        LoggerFactory.getLogger(EmployeeController.class);
+
+    @Autowired
+    private IEmployeeService employeeService;
 }
