d1ee5931a905a69daca0aab72cb42745d2b5342e
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2023-12-25 14:55:01 -0600

    adding bootstrap to the reacts app

--- human-resources-app/src/App.js
+++ human-resources-app/src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <div>
-      <h1>Human Resources System</h1>
+    <div className="container text-center">
+      <h3>Human Resources System</h3>
     </div>
   );
 }
