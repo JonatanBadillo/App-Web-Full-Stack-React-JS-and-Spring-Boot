c8f2af5f2cb0410eee73763ecbc5ce48870e9e55
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2023-12-25 17:57:02 -0600

    component Routes in React

--- human-resources-app/src/App.js
+++ human-resources-app/src/App.js
@@ -1,11 +1,16 @@
+import { BrowserRouter , Route, Routes} from "react-router-dom"; 
 import ListEmployees from "./employees/ListEmployees";
 import Nav from "./template/Nav";
 
 function App() {
   return (
     <div className="container">
-      <Nav/>
-      <ListEmployees/>
+      <BrowserRouter>
+        <Nav/>
+        <Routes>
+          <Route exact path='/' element={<ListEmployees/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
     
   );
